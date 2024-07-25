72af9460ebee9d2bcdc464af9221acfd60b0c2b7
Sofiane74100 <arfane.sofiane@gmail.com>
2024-07-26 01:04:27 +0200

    s

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,14 @@
-function handleResponseFromAPI(promise) {
-    return promise
-      .then(() => ({
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
         status: 200,
         body: 'success',
-      }))
-      .catch(() => new Error())
-      .finally(() => {
-        console.log('Got a response from the API');
-      });
-  }
-  
-  export default handleResponseFromAPI;
+      };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
+}
