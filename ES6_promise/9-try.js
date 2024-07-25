7cdd54eba469931fffde6898f1721943cd122f7d
Sofiane74100 <arfane.sofiane@gmail.com>
2024-07-26 01:28:15 +0200

    r

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,17 +1,14 @@
-function guardrail(mathFunction) {
-    const queue = [];
-    try {
-      // Attempt to execute the mathFunction and push the result to the queue
-      const result = mathFunction();
-      queue.push(result);
-    } catch (error) {
-      // If an error occurs, push the error message to the queue
-      queue.push(`Error: ${error.message}`);
-    } finally {
-      // Regardless of the outcome, push this message to the queue
-      queue.push('Guardrail was processed');
-    }
-    return queue;
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  
-  export default guardrail;
+
+  return queue;
+}
